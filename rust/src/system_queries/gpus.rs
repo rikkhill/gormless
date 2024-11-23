4c0c142085d45a9e01207ffb51df02869ac42660
Rikk Hill <rikk@unashamedly.co.uk>
2024-11-23 00:48:56 +0000

    Educational foolishness with Rust and OCL queries

--- rust/src/system_queries/gpus.rs
+++ rust/src/system_queries/gpus.rs
@@ -1,22 +1,37 @@
-use ocl::{Platform, Device, Result as OclResult};
+use ocl::{Platform, Device};
 use std::error::Error;
+use std::any::Any;
+use std::panic;
 
 pub fn has_gpus() -> Result<bool, Box<dyn Error>> {
+
+    let original_hook = panic::take_hook();
+    panic::set_hook(Box::new(|_info| {
+        // Do nothing on panic
+    }));
+
     // Get all available OpenCL platforms
-     // Try to list OpenCL platforms
-    let platforms_result: OclResult<Vec<Platform>> = Ok(Platform::list());
+    // Try to list OpenCL platforms
+    let platforms_result: Result<Vec<Platform>, Box<dyn Any + Send>> = std::panic::catch_unwind(|| {
+        Platform::list()
+    });
+
+    panic::set_hook(original_hook);
 
     match platforms_result {
         Ok(platforms) if !platforms.is_empty() => {
-            Ok(true) // No GPU devices found, even though platforms exist
+            //Ok(true) // No GPU devices found, even though platforms exist
+            Ok(true)
         }
         Ok(_) => {
             // No platforms found
+            //Ok(false)
             Ok(false)
         }
-        Err(e) => {
+        Err(_e) => {
             // Error retrieving platform list (e.g., OpenCL not supported or installed)
-            Err(Box::new(e))
+            //Err(Box::new(e))
+            Ok(false)
         }
     }
 }
